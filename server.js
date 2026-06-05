75eb02c803fac5194ea96b58ab70b8180719d071
aalsawalmeh <aalsawalmeh1986@gmail.com>
2026-06-05 16:50:26 +0300

    Use highly reliable Google and Twilio STUN servers to fix ICE timeouts

--- server.js
+++ server.js
@@ -279,16 +279,8 @@ app.get('/control', (req, res) => {
           iceServers: [
             { urls: 'stun:stun.l.google.com:19302' },
             { urls: 'stun:stun1.l.google.com:19302' },
-            {
-              urls: "turn:openrelay.metered.ca:80",
-              username: "openrelayproject",
-              credential: "openrelayproject"
-            },
-            {
-              urls: "turn:openrelay.metered.ca:443",
-              username: "openrelayproject",
-              credential: "openrelayproject"
-            }
+            { urls: 'stun:stun2.l.google.com:19302' },
+            { urls: 'stun:global.stun.twilio.com:3478' }
           ] 
         });
         peerConnections[sid] = pc;
